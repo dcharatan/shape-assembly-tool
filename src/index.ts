82f2ec2ee6bcad40dc17d5c779c05b4faeeafa41
David Charatan <david.charatan@gmail.com>
2020-09-12 17:25:45 -0700

    Add transpiler functionality.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('ShapeAssembly');
+import * as fs from 'fs';
+import { TranspilerInverse } from '@dcharatan/shape-assembly-parser';
+
+// Get the arguments.
+const userArguments = process.argv.slice(2);
+if (userArguments.length !== 1) {
+  console.error('You must specify the input file.');
+  process.exit(1);
+}
+
+const inFile = userArguments[0];
+const program = fs.readFileSync(inFile).toString();
+const transpiler = new TranspilerInverse();
+console.log(transpiler.transpile(program));
